157cd4c8c39cb7964b7da792a00e2211587ef946
= <=>
2025-04-07 16:58:17 -0300

    find followers

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
@@ -38,20 +39,37 @@ class InstaFollower:
 
         time.sleep(4.5)
         # Click not now and ignore save-login info
-        save_login_prompt = self.driver.find_element(By.XPATH, value="//div[contains(text(), 'Not now')]")
-        if save_login_prompt:
-            save_login_prompt.click()
+        try:
+            save_login_prompt = self.driver.find_element(By.XPATH, value='/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div/div/div/div')
+            if save_login_prompt:
+                save_login_prompt.click()
+        except NoSuchElementException:
+            print("Save login prompt not found or already dismissed.")
 
         time.sleep(4)
 
         # Click not now for notifications
-        notifications_prompt = self.driver.find_element(By.XPATH, value="// button[contains(text(), 'Not Now')]")
+        notifications_prompt = self.driver.find_elements(By.XPATH, value="//button[contains(text(), 'Not Now')]")
         if notifications_prompt:
-            notifications_prompt.click()
+            notifications_prompt[0].click()
+
 
     def find_followers(self):
-        pass
+        time.sleep(5)
+        # Show people following the selected account
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}/followers")
 
+        time.sleep(5.5)
+
+        # The Xpath can change in the future, so make sure to update if needed.
+        modal_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]"
+        modal = self.driver.find_element(By.XPATH, value=modal_xpath)
+
+        for i in range(10):
+            # This script will use Javascript to scroll the
+            # top of the modal element by the height of the modal
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
     def follow(self):
         pass
 
