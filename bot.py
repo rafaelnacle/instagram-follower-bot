b75cca779fab4ac5f2a3a2cfeb9644d8de2c8a1b
= <=>
2025-04-07 16:21:56 -0300

    create webdriver on init

--- bot.py
+++ bot.py
@@ -1,2 +1,17 @@
+from selenium import webdriver
+
 class InstaFollower:
-    pass
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
